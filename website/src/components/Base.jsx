be680d899c9f0af445f4822c4f4ec2bf014a8d2b
Vihanga Theekshana <theekshanavihanga2@gmail.com>
2025-10-11 23:40:12 +0530

    navbar edit

--- website/src/components/Base.jsx
+++ website/src/components/Base.jsx
@@ -2,15 +2,19 @@
 import React from "react";
 import { SparklesCore } from "./ui/sparkles";
 import Navbar from "./Navbar";
-
+import LiquidEther from "./Background";
 export function Base() {
   return (
     <>
-      <Navbar />
-      <div className="h-[30rem] w-full bg-black flex flex-col items-center pt-10  overflow-hidden ">
+      <div className="z-50 fixed ">
+        <Navbar />
+      </div>
+
+      <div className="h-[30rem] w-full bg-black flex flex-col items-center pt-30">
         <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center bg-linear-to-r from-cyan-400 via-blue-400 to-indigo-400 bg-clip-text text-transparent relative z-20">
           Welcome to Deepthink
         </h1>
+
         <div className="w-[60rem] h-40 relative">
           {/* Gradients */}
           <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
@@ -31,6 +35,35 @@ export function Base() {
           {/* Radial Gradient to prevent sharp edges */}
           <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         </div>
+        <p className="text-cyan-500 font-semibold text-center text-lg w-1/2 flex-wrap">
+          DeepThink is a visionary AI-driven web development agency dedicated to
+          creating intelligent digital solutions. We design sleek,
+          high-performance websites, build powerful software systems, and
+          integrate advanced AI technologies to accelerate business growth. From
+          modern web design to smart automation, DeepThink empowers your brand
+          to thrive in the digital era with innovation and precision.
+        </p>
+      </div>
+
+      <div style={{ width: "100%", height: 600, position: "relative" }}>
+        <LiquidEther
+          colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+          className="bg-black"
+        />
       </div>
     </>
   );
--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -6,28 +6,28 @@ import { LuMail } from "react-icons/lu";
 const Navbar = () => {
   return (
     <>
-      <nav className="flex w-full h-20 bg-black  items-center justify-between pl-5 pr-7">
+      <nav className="flex w-full h-20 bg-black  items-center justify-between pl-5 pr-7 fixed">
         <div className="bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent text-2xl font-bold ">
           &#123; Deepthink &#125;
         </div>
         <div className="flex  gap-5  text-indigo-400">
-          <button className="focus:border-2 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2  transition  duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
+          <button className="focus:bg-gray-800 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2  transition  duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
             <AiOutlineHome />
             Home
           </button>
-          <button className="focus:border-2 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2  transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
+          <button className="focus:bg-gray-800 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2  transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
             <MdOutlineHomeRepairService />
             Services
           </button>
-          <button className="focus:border-2 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2 transition  duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
+          <button className="focus:bg-gray-800 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2 transition  duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
             <FaRegFile />
             Portfolio
           </button>
-          <button className="focus:border-2 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2  transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
+          <button className="focus:bg-gray-800 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2  transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
             <FaRegUser />
             About
           </button>
-          <button className="focus:border-2 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2 transition  duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
+          <button className="focus:bg-gray-800 rounded-2xl cursor-pointer px-2 py-1 flex items-center gap-2 transition  duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
             <LuMail />
             Contact
           </button>
