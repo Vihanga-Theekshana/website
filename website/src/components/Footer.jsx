28a19d9017a804c5f6afe725a1d40ee142c7ddf9
Vihanga Theekshana <theekshanavihanga2@gmail.com>
2025-10-27 13:20:46 +0530

    add footer

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { LuFacebook, LuTwitter } from "react-icons/lu";
+import { FaInstagram } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-white">
@@ -9,12 +11,24 @@ export default function Footer() {
           ></img>
 
           <div>
-            <div></div>
+            <div className="w-full md:w-1/3  pl-13 p-3 flex flex-col items-center">
+              <ul className="flex gap-4 pt-3">
+                <li>
+                  <LuFacebook className="text-2xl cursor-pointer hover:text-cyan-400 transition duration-500" />
+                </li>
+                <li>
+                  <LuTwitter className="text-2xl cursor-pointer  hover:text-cyan-400 transition duration-500" />
+                </li>
+                <li>
+                  <FaInstagram className="text-2xl cursor-pointer  hover:text-cyan-400 transition duration-500" />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="flex-1 flex items-start md:justify-end gap-20">
           <div>
-            <h2 className="font-semibold mb-5 text-gray-800">Company</h2>
+            <h2 className="font-semibold mb-5 text-white">Company</h2>
             <ul className="text-sm space-y-2">
               <li>
                 <a href="#">Home</a>
@@ -31,7 +45,7 @@ export default function Footer() {
             </ul>
           </div>
           <div>
-            <h2 className="font-semibold mb-5 text-gray-800">Get in touch</h2>
+            <h2 className="font-semibold mb-5 text-white">Get in touch</h2>
             <div className="text-sm space-y-2">
               <p>078-5999845</p>
               <p>info@zyntexait.com</p>
@@ -40,7 +54,7 @@ export default function Footer() {
         </div>
       </div>
       <p className="pt-4 text-center text-xs md:text-sm pb-5">
-        Copyright 2024 © <a href="https://prebuiltui.com">PrebuiltUI</a>. All
+        Copyright 2024 © <a href="https://prebuiltui.com">ZyntexaIT</a>. All
         Right Reserved.
       </p>
     </footer>
